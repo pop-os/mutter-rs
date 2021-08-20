0ce0420c0895885d98b880effd8cf73fcc269bc0
Jeremy Soller <jeremy@system76.com>
2021-08-20 15:59:43 -0600

    Add generate scripts

---
+++ generate.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -ex
+
+pushd mutter-gir-files
+rm -fv *.gir
+cp -v /usr/share/gir-1.0/GDesktopEnums-3.0.gir . && patch -p1 < GDesktopEnums-3.0.patch
+cp -v /usr/share/gir-1.0/Json-1.0.gir .
+cp -v /usr/share/gir-1.0/xlib-2.0.gir . && patch -p1 < xlib-2.0.patch
+cp -v /usr/lib/x86_64-linux-gnu/mutter-8/Cogl-8.gir . && patch -p1 < Cogl-8.patch
+cp -v /usr/lib/x86_64-linux-gnu/mutter-8/CoglPango-8.gir .
+cp -v /usr/lib/x86_64-linux-gnu/mutter-8/Clutter-8.gir . && patch -p1 < Clutter-8.patch
+cp -v /usr/lib/x86_64-linux-gnu/mutter-8/Meta-8.gir . && patch -p1 < Meta-8.patch
+popd
+
+# Packages to generate, listed in dependency order
+pkgs=(
+    xlib-sys
+    cogl-sys
+    atk-sys
+    json-sys
+    clutter-sys
+    gdesktop_enums-sys
+    meta-sys
+)
+
+for pkg in "${pkgs[@]}"
+do
+    if [ ! -d "${pkg}" ]
+    then
+        cargo run --release --manifest-path gir/Cargo.toml -- \
+            --config "${pkg}.toml" \
+            --girs-directories mutter-gir-files \
+            --girs-directories gir-files
+        if [ -f "${pkg}.patch" ]
+        then
+            pushd "${pkg}"
+            patch -p1 < "../${pkg}.patch"
+            popd
+        fi
+    fi
+    cargo build --release --manifest-path "${pkg}/Cargo.toml" --all-features
+done
