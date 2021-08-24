4d41507439f006fa14a6498b69b95a76bde41c29
Jeremy Soller <jeremy@system76.com>
2021-08-24 15:18:34 -0600

    Improve Meta Rectangle implementation

--- meta/src/rectangle.rs
+++ meta/src/rectangle.rs
@@ -1,10 +1,40 @@
-use glib::translate::Uninitialized;
+use ffi::MetaRectangle;
+use glib::translate::{
+    FromGlibPtrNone,
+    Uninitialized,
+    ToGlibPtr,
+};
 use std::mem;
 
 use crate::Rectangle;
 
+impl Rectangle {
+    pub fn x(&self) -> libc::c_int {
+        unsafe { (*self.to_glib_none().0).x }
+    }
+
+    pub fn y(&self) -> libc::c_int {
+        unsafe { (*self.to_glib_none().0).y }
+    }
+
+    pub fn width(&self) -> libc::c_int {
+        unsafe { (*self.to_glib_none().0).width }
+    }
+
+    pub fn height(&self) -> libc::c_int {
+        unsafe { (*self.to_glib_none().0).height }
+    }
+}
+
 impl Uninitialized for Rectangle {
     unsafe fn uninitialized() -> Self {
-        mem::zeroed()
+        //TODO: is this implementation correct?
+        let rect = MetaRectangle {
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+        };
+        Self::from_glib_none(&rect as *const MetaRectangle)
     }
 }
